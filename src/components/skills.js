f4ee1ae2fb883c780d0bd510fc9dc308b2f6f60e
Katie Liu <katieliu59@gmail.com>
2023-03-17 00:13:28 -0400

    Format code

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,52 +1,58 @@
 export default function Skills() {
-    return(<div class="container-fluid">
-    <div id="skills" class="row">
-        <div class="center-content col-lg-3 d-flex justify-content-center">
-            <div class="description">
-                <h2 class="description">Languages</h2>
-                <p>
-                    JavaScript <br />
-                    HTML <br />
-                    CSS & Sass <br />
-                    PostgreSQL
-                </p>
-            </div>
-        </div>
-        <div class="center-content col-lg-3 d-flex justify-content-center">
-            <div class="description">
-                <h2 class="description">Libraries</h2>
-                <p> jQuery <br />
-                    Axios <br />
-                    React.js <br />
-                    Node.js <br />
-                    Express.js <br />
-                    Bootstrap
-                </p>
-
-            </div>
-        </div>
-        <div class="center-content col-lg-3 d-flex justify-content-center">
-            <div class="description">
-                <h2 class="description">Tools</h2>
-                <p>
-                    Git & Github <br />
-                    Netlify</p>
-
-            </div>
-        </div>
-        <div class="center-content col-lg-3 d-flex justify-content-center">
-            <div class="description">
-                <h2 class="description">UI Design</h2>
-                <p>  Figma <br />
-                Photoshop <br />
-                Lightroom <br />
-                Wireframing <br />
-                Mockup
-                </p>
-              
-            </div>
-        </div>
-    </div>
-    </div>
-    )
+	return (
+		<div class="container-fluid">
+			<div
+				id="skills"
+				class="row"
+			>
+				<div class="center-content col-lg-3 d-flex justify-content-center">
+					<div class="description">
+						<h2 class="description">Languages</h2>
+						<p>
+							JavaScript <br />
+							HTML <br />
+							CSS & Sass <br />
+							PostgreSQL
+						</p>
+					</div>
+				</div>
+				<div class="center-content col-lg-3 d-flex justify-content-center">
+					<div class="description">
+						<h2 class="description">Libraries</h2>
+						<p>
+							{" "}
+							jQuery <br />
+							Axios <br />
+							React.js <br />
+							Node.js <br />
+							Express.js <br />
+							Bootstrap
+						</p>
+					</div>
+				</div>
+				<div class="center-content col-lg-3 d-flex justify-content-center">
+					<div class="description">
+						<h2 class="description">Tools</h2>
+						<p>
+							Git & Github <br />
+							Netlify
+						</p>
+					</div>
+				</div>
+				<div class="center-content col-lg-3 d-flex justify-content-center">
+					<div class="description">
+						<h2 class="description">UI Design</h2>
+						<p>
+							{" "}
+							Figma <br />
+							Photoshop <br />
+							Lightroom <br />
+							Wireframing <br />
+							Mockup
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
