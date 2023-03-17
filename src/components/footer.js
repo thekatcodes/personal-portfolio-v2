b7d48807184f2ac87d75ae306a54228e1b54fc3d
Katie Liu <katieliu59@gmail.com>
2023-03-16 23:59:32 -0400

    Add footer component

---
+++ src/components/footer.js
@@ -0,0 +1,32 @@
+export default function Footer() {
+    return (
+        <footer class="about-footer">
+            <div class="container footer-container">
+                <div class="row d-flex align-items-center">
+                    <div class="col-lg-8 pt-3">
+                        <p class="footer-text">
+                            Designed and developed with 💜 by
+                            <a href="https://www.linkedin.com/in/katiejxliu/" target="_blank"
+                                class="open-source-link"><b>Katie Liu</b></a>, and
+                            <a href="https://github.com/thekatcodes/portfolio-project" target="_blank"
+                                class="open-source-link">
+                                <b>open-sourced</b>
+                            </a>
+                        </p>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="footer-icons">
+                            <a href="https://github.com/thekatcodes" target="_blank">
+                                <i class="footer-github github-icon fa-brands fa-github"></i></a>
+                            <a href="https://www.linkedin.com/in/katiejxliu/" target="_blank"><i
+                                class="footer-linkedin fa-brands fa-linkedin"></i></a>
+                            <a href="mailto:katieliu59@gmail.com"><i class="footer-email fa-solid fa-envelope"></i></a>
+                            <a href="files/Katie_Liu_CV_2023.pdf" target="_blank" title="CV/Resume">
+                                <i class="footer-cv fa-solid fa-file-lines"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
