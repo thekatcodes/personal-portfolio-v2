b701001927c1bcfcab9b00377c040d9e00c55579
Katie Liu <katieliu59@gmail.com>
2023-03-23 23:51:59 -0400

    Update code to display logo

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
     return (
         <nav class="navbar navbar-expand-lg navbar-light" id="top">
-            <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt="Logo image" class="logo" /></a>
+            <a class="navbar-brand" href="index.html"><img src="../images/logo.png" alt="Logo image" class="logo" /></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
