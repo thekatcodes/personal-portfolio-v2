7d5b4c689b4539e5d0afe9fae916ca30de3ebbcb
Katie Liu <katieliu59@gmail.com>
2023-05-02 23:38:20 -0400

    Create data objects for pokedex and snazzy snacks

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,1 +1,46 @@
-export const projects = [{}]
+const pokedex = "src/images/pokedex.png";
+const snazzysnacks = 'src/images/snazzysnacks.png'
+
+export const projects = [
+	{
+		id: 1,
+		title: "Pokedex",
+		description:
+			"Pokedex is a fully responsive single-page app that allows users to search for pokemon in the entire pokemon directory. Users can filter their search by pokemon type as well. This project is built using Next.js, Axios, PokeAPI and Tailwind CSS.",
+		skills: ["HTML5", "CSS3", "Next.js", "React.js", "Javascript", "API"],
+		image: {
+			src: pokedex,
+			alt: "Pokedex thumbnail",
+		},
+		links: {
+			github: "https://github.com/thekatcodes/pokedex",
+		},
+	},
+	{
+		id: 2,
+		title: "Snazzy Snacks",
+		description:
+			"Snazzy Snacks is a full-stack React app that offers monthly subscriptions of randomized snack selections based on three tiers. This project was built in a team environment for the final project at Lighthouse Labs. This project is built with React, React-Router-Dom, Axios, SweetAlert2, SCSS, Node.js, Express, PostgreSQL, Stripe, Bcrypt and designed with Figma.",
+		skills: [
+			"HTML5",
+			"CSS3",
+			"SASS",
+			"React.js",
+			"React-Router",
+			"Javascript",
+			"Node.js",
+			"Express.js",
+			"PostgreSQL",
+			"Stripe",
+			"Bcrypt",
+			"Figma",
+		],
+		image: {
+			src: snazzysnacks,
+			alt: "Snazzy Snacks thumbnail",
+		},
+		links: {
+			github: "https://github.com/thekatcodes/snazzy-snacks",
+		},
+	},
+];
