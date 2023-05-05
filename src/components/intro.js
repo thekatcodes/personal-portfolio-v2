ff01e8637c7fdf38b0a2d0fa17f5869fa7a8d8ac
Katie Liu <katieliu59@gmail.com>
2023-05-04 23:45:06 -0400

    Remove unused code

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,23 +1,23 @@
 import React from 'react';
 export default function Intro() {
     return (
-        <section class="home-page">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-8 fade-in-text">
-                        <h1 class="intro">Hi there! I'm Katie Liu</h1>
-                        <h2 class="description">
+        <section>
+            <div>
+                <div>
+                    <div>
+                        <h1>Hi there! I'm Katie Liu</h1>
+                        <h2>
                             Front-end developer based in Canada
-                            <i class="fa-brands fa-canadian-maple-leaf"></i>
+                            {/* <i class="fa-brands fa-canadian-maple-leaf"></i> */}
                         </h2>
                     </div>
-                    <div class="col-lg-4 d-flex justify-content-center fade-in-text">
-                        <div class="ellipse"></div>
-                        <img src="images/profilepic.png" alt="Profile picture" class="profile-pic" />
+                    <div>
+                        <div></div>
+                        {/* <img src="images/profilepic.png" alt="Profile picture" class="profile-pic" /> */}
                     </div>
                 </div>
             </div>
-            <div class="arrow" id="arrow-down">
+            <div>
                 <a href="index.html#summary-header" class="arrow-link"><i class="fa-solid fa-angles-down"></i></a>
             </div>
         </section>
