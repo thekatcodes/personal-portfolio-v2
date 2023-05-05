9fce1b822df539b4d8b9d4de174ab00629095049
Katie Liu <katieliu59@gmail.com>
2023-05-04 23:49:25 -0400

    Update navbar code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,19 @@
-import { AnchorLink } from "gatsby-plugin-anchor-links";
+// import { AnchorLink } from "gatsby-plugin-anchor-links";
 import React from "react";
-import logo from '../images/logo.png'
+import logo from "../images/logo.png";
 
 export default function Header() {
-    return (
-        <nav>
-            <img src={logo} alt="Logo image" className="logo" style={{width:35 + 'px'}}/>
-            <div>
-            <AnchorLink to="#about" title="About">About</AnchorLink>
-
-
-         </div>
-        </nav>
-    )
+	return (
+		<nav>
+			<img
+				src={logo}
+				alt="Logo image"
+				className="logo"
+				style={{ width: 35 + "px" }}
+			/>
+			<div>
+				<a>About</a>
+			</div>
+		</nav>
+	);
 }
