eddad793f414a74655c21c4113c6783dd710576e
Katie Liu <katieliu59@gmail.com>
2023-05-04 23:48:08 -0400

    Make temporary logo width for better visibility

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import logo from '../images/logo.png'
 export default function Header() {
     return (
         <nav>
-            <img src={logo} alt="Logo image" class="logo" />
+            <img src={logo} alt="Logo image" className="logo" style={{width:35 + 'px'}}/>
             <div>
             <AnchorLink to="#about" title="About">About</AnchorLink>
 
